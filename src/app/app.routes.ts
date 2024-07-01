daa39dcdfcdfb1e51e4f61181c7b34844533b9bf
MuzamilAli-44 <muzamil.ali@aerodyne.group>
2024-07-01 16:52:52 +0500

    Nothing changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'search', loadChildren:()=> import('./modules/search/search.module').then((m)=> m.SearchModule)
+        path: 'search', loadChildren:()=> import('./modules/search/search.module').then((m)=> m.SearchModule), 
     },
     {
         path: 'categories', loadChildren:()=> import('./modules/category/category.module').then((m)=>m.CategoryModule)
